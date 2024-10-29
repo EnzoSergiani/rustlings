a10e8e0acabb1271f792f718fa9af65a4ad9db7a
Enzo Sergiani <enzo.sergiani@protonmail.com>
2024-10-29 12:35:10 +0100

    if done

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -3,11 +3,13 @@ fn animal_habitat(animal: &str) -> &str {
     let identifier = if animal == "crab" {
         1
     } else if animal == "gopher" {
-        2.0
+        2
     } else if animal == "snake" {
         3
+    } else if animal == "dinosaur" {
+        4
     } else {
-        "Unknown"
+        0
     };
 
     // Don't change the expression below!
