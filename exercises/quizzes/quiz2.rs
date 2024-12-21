afcf51b67fe3a870f56cbd70e634a455e11db59c
Enzo Sergiani <enzo.sergiani@protonmail.com>
2024-12-21 13:37:18 +0100

    quizzes done

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -24,10 +24,23 @@ enum Command {
 }
 
 mod my_module {
+    use std::{array, vec};
+
     use super::Command;
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut result = Vec::new();
+        for (string, command) in input {
+            match command {
+                Command::Uppercase => result.push(string.to_uppercase()),
+                Command::Trim => result.push(string.trim().to_string()),
+                Command::Append(times) => result.push(format!("{}{}", string, "bar".repeat(times))),
+            }
+        }
+        result   
+    }
 }
 
 fn main() {
@@ -37,7 +50,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
